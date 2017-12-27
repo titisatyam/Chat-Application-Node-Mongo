6c30331dd9183e10c99b3daf326e658547237aac
vikash singh <vikash@apiria.com>
2017-12-27 19:24:10 +0530

    change to message to name

--- server/server.js
+++ server/server.js
@@ -50,7 +50,7 @@ io.on('connection', (socket) => {
         var user = users.getUser(socket.id);
 
         if (user && isRealString(message.text)) {
-            io.to(user.room).emit('newMessage', generateMessage(message.name, message.text));
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
         }
 
         callback('This is from the server.');
